bd2699e9006c0a2aed29782314ecbee60f2e9532
Tanveer (Tanny) <39107961+TannyBhoy@users.noreply.github.com>
2019-04-25 23:21:50 +0100

    bug fixes and programming convention updates

--- Dealership.java
+++ Dealership.java
@@ -1,82 +1,163 @@
-
-
+//*
+//*
+//stepOne
+//*
+//*
 public class Dealership {
-    
-    private class DealershipNode{
+
+    public class NameNotUniqueException extends Exception {
+    }
+
+    public class NameNotFoundException extends Exception {
+    }
+
+//    public class NotValidInputException extends Exception {
+//    }
+    private class DealershipNode {
+
         private String name;
         private DealershipNode nextMake;
     }
-      
+
     private DealershipNode make;
     private DealershipNode current;
-    
-    protected Dealership(String carMake){
+    private DealershipNode previous;
+    private DealershipNode next;
+
+    protected Dealership(String carMake) {
         this.make = new DealershipNode();
         this.make.name = carMake;
         this.current = this.make;
     }
-    
-    public void addMake(String carMake){
+
+    public void addMake(String carMake) throws NameNotUniqueException {
         DealershipNode newMake = new DealershipNode();
         newMake.name = carMake;
-        
-        if (this.current.nextMake == null){
+
+        if (this.current.nextMake == null) {
             this.current.nextMake = newMake;
-        }else{
+        } else {
             DealershipNode currentMake = this.current.nextMake;
-            while(currentMake.nextMake != null){
+            if (current.nextMake.name.equalsIgnoreCase(carMake)) {
+                throw new NameNotUniqueException();
+            }
+            while (currentMake.nextMake != null) {
                 currentMake = currentMake.nextMake;
             }
             currentMake.nextMake = newMake;
         }
     }
-    
-    public void search(String carMake){
-        this.current = this.make;
-        
-        if(carMake.equals(this.make.name)){
-            make = this.make;
-            System.out.println("\nSuccess! \n" + "'" + carMake.toUpperCase() + "'" + " Found\n");
-            this.current = make;
-        }else if(this.make.nextMake == null){
-            System.out.println("Not Found");
-        }else{
-            make = this.make.nextMake;
-            
-            while (make != null){
-                
-                if(carMake.equals(make.name)){                   
-                   System.out.println("\nSuccess! \n" + "'" + carMake.toUpperCase() + "'" + " Found\n");
+
+    public void option(Integer option) throws NameNotUniqueException {
+
+    }
+
+    public void search(String carMake) throws NameNotFoundException {
+        DealershipNode currentVehicle = this.make.nextMake;
+        boolean searchOngoing = true;
+
+        while (searchOngoing) {
+            if (carMake.equals(currentVehicle.name)) {
+                currentVehicle = this.make;
+                System.out.println("\nSuccess! \n" + "'" + carMake.toUpperCase() + "'" + " Found\n");
+                this.current = currentVehicle;
+                searchOngoing = false;
+            } else {
+                if (currentVehicle.nextMake == null) {
+                    System.out.println("'" + carMake.toUpperCase() + "'" + " Not Found\n");
+                    searchOngoing = false;
+                } else {
+                    currentVehicle = currentVehicle.nextMake;
+
+                    if (current.nextMake.name.equals(carMake)) {
+                        throw new NameNotFoundException();
+                    }
                 }
-                make = make.nextMake;
             }
-            this.current = make;
         }
     }
-    
-//    public void remove(){
-//        
+
+    public void remove(String carMake) throws NameNotFoundException {
+        DealershipNode currentVehicle = this.make;
+        boolean searchOngoing = true;
+
+        while (searchOngoing) {
+            if (carMake.equals(currentVehicle.name)) {
+                currentVehicle = this.make;
+                System.out.println("\nSuccess! \n" + "'" + carMake.toUpperCase() + "'" + " Found\n");
+                this.current = currentVehicle;
+                searchOngoing = false;
+            } else {
+                if (currentVehicle.nextMake == null) {
+                    System.out.println("'" + carMake.toUpperCase() + "'" + " Not Found\n");
+                    searchOngoing = false;
+                } else {
+                    currentVehicle = currentVehicle.nextMake;
+                    if (current.nextMake.name.equals(carMake)) {
+                        throw new NameNotFoundException();
+                    }
+                }
+            }
+        }
+        if (carMake.equals(currentVehicle.name)) {
+            System.out.println("Match");
+            this.previous = this.current;
+            this.current = this.next;
+        }
+
+    }
+
+//    public boolean search(String carMake) throws NameNotFoundException {
+//        this.current = this.make;
+//        boolean exists = false;
+//        if (carMake.equals(this.make.name)) {
+////            make = this.make;
+//            exists = true;
+//            System.out.println("\nSuccess! \n" + "'" + carMake.toUpperCase() + "'" + " Found\n");
+////            this.current = make;
+//        } else if (this.make.nextMake == null) {
+//            System.out.println("'" + carMake.toUpperCase() + "'" + " Not Found\n");
+//        } else {
+//            if (current.nextMake.name.equals(carMake)) {
+//                throw new NameNotFoundException();
+//            }
+//            make = this.make.nextMake;
+//
+//            while (make != null) {
+//
+//                if (carMake.equals(make.name)) {
+//                    System.out.println("\nSuccess! \n" + "'" + carMake.toUpperCase() + "'" + " Found\n");
+//                }
+//                make = make.nextMake;
+//            }
+//            this.current = make;
+//        }
+//        return (exists);
 //    }
-    
-    public String toString(){
+//
+//    public boolean remove(String carMake) throws NameNotFoundException {
+//        System.out.println(carMake + "testError");
+//        boolean car = search(carMake);
+//        System.out.println(car + "testFound");
+//        return (car);
+//    }
+    @Override
+    public String toString() {
         String dealershipDetails = new String();
         this.current = this.make;
-        
-        dealershipDetails += this.current.name +"\n";
+
+        dealershipDetails += this.current.name + "\n";
         DealershipNode nextMake = this.current.nextMake;
-        
-        if (this.current.nextMake == null){
-            dealershipDetails += "\n";
-        }
-        else {
-            while (nextMake != null){
-                dealershipDetails += nextMake.name + "\n";
-                
+
+        if (this.current.nextMake == null) {
+            dealershipDetails += " \tNo make entered!\n";
+        } else {
+            while (nextMake != null) {
+                dealershipDetails += "\t" + nextMake.name + "\n";
+
                 nextMake = nextMake.nextMake;
             }
         }
         return dealershipDetails;
     }
 }
-
-
--- DealershipTest.java
+++ DealershipTest.java
@@ -1,17 +1,23 @@
-
-
+//*
+//*
+//stepOne
+//*
+//*
 public class DealershipTest {
 
-
-    public static void main (String args[]){
-        String carMake = Input.getString("Enter make: ");
-        Dealership carList = new Dealership(carMake);
+    public static void main(String args[]) {
+        String DealershipName = Input.getString("Enter Dealership Name: ");
+        System.out.println("\nWelcome to " + "'" + DealershipName.toUpperCase() + "'" + " Dealership\n");
+        System.out.println("Please select an option from the menu\n");
+        String carMake = "";
+        Dealership carList = new Dealership(DealershipName);
         Integer option;
         do {
             System.out.println("0: Quit");
-            System.out.println("1: Add Make");         
+            System.out.println("1: Add Make");
             System.out.println("2: Display");
             System.out.println("3: Search");
+            System.out.println("4: Remove Make\n");
             option = Input.getInteger("input option: ");
             System.out.println();
             switch (option) {
@@ -19,21 +25,42 @@ public class DealershipTest {
                     System.out.println("Quitting Program");
                     break;
                 case 1:
-                    carMake = Input.getString("Enter new make: ");
-                    carList.addMake(carMake);
+                    try {
+                        carMake = Input.getString("Enter new make: ");
+                        carList.addMake(carMake);
+                        System.out.println("'" + carMake.toUpperCase() + "'" + " added\n");
+                    } catch (Dealership.NameNotUniqueException e) {
+                        System.out.println("\n\tError! " + "'" + carMake.toUpperCase() + "'" + " already exists\n");
+                        System.out.println("\tPlease enter a new Make\n");
+                    }
                     break;
                 case 2:
                     System.out.println(carList);
-                    break;     
+                    break;
                 case 3:
-                    carMake = Input.getString("Search make: ");
-                    carList.search(carMake);
-                   
-                    break; 
+                    try {
+                        carMake = Input.getString("Search make: ");
+                        carList.search(carMake);
+                    } catch (Dealership.NameNotFoundException e) {
+                        System.out.println("\n\tError! " + "'" + carMake.toUpperCase() + "'" + " does not exist\n");
+                        System.out.println("\tPlease enter a new Search\n");
+                    }
+                    break;
+                case 4:
+                    try {
+                        carMake = Input.getString("Search make: ");
+                        carList.remove(carMake);
+                    } catch (Dealership.NameNotFoundException e) {
+                        System.out.println("\n\tError! " + "'" + carMake.toUpperCase() + "'" + " does not exist\n");
+                        System.out.println("\tPlease enter a new Search\n");
+                    }                    
+                    break;
                 default:
-                    System.out.println("invalid option");
+                    System.out.println("\t**INVALID OPTION**\n");
+                    System.out.println("\tPlease select a valid option from the menu below:\n");
             }
         } while (option != 0);
     }
-    
+
 }
+
